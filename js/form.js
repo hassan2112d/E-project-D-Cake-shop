ace14b8cebd375aa70f56fce5d44928d380c9150
hassan2112d <hassan2112d@aptechgdn.net>
2024-01-18 22:06:34 +0500

    hello

---
+++ js/form.js
@@ -0,0 +1,14 @@
+function login()
+{
+  alert("Your Login Successfully")
+}
+
+function oder()
+{
+  alert("Your oder has been place")
+}
+
+function signup()
+{
+  alert("Your Account Has Been Created")
+}
